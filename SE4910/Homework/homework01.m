eda6a7683e4a65a3cdd997fb48a59f73f4e12f6c
Steve Mazza <mazzas@cantontechnology.com>
2013-07-12 15:45:45 -0400

    Cleaned up comments.

--- SE4910/Homework/homework01.m
+++ SE4910/Homework/homework01.m
@@ -10,7 +10,6 @@
 num = [1 5 6 9 30];
 den = [1 6 21 46 30];
 [r,p,k] = residue(num,den)
-% TODO: format output.
 
 syms s
 F = (s^4+5*s^3+6*s^2+9*s+30)/(s^4+6*s^3+21*s^2+46*s+30);
@@ -24,16 +23,13 @@ p = [0; -4; -6; 2+3i; 2-3i];
 k = 5;
 [num,den] = zp2tf(z,p,k);
 printsys(num,den,'s')
-% TODO: format output.
 
 %% Problem 5
 % Also see attached PDF.
 syms w
 F = 5/(s^2*(s^2+w^2));
 ilaplace(F)
-% TODO: format output.
 
 %% Problem 6
 % Also see attached PDF.
 dsolve('D2x+3*Dx+2*x=0','x(0)=-1, Dx(0)=2')
-% TODO: format output.
