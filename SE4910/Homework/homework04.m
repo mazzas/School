7d38697c0ffa1db88eb5e73da8c375ec0420e2d8
Steve Mazza <mazzas@cantontechnology.com>
2013-08-04 18:02:25 -0400

    More work.

--- SE4910/Homework/homework04.m
+++ SE4910/Homework/homework04.m
@@ -1,5 +1,6 @@
 %% Homework 7, Problem 1, a
-%subplot(3, 3, 1);
+clear all
+s = tf('s');
 num = [1];
 den = [1 0 0 0];
 rlocus(num,den)
@@ -7,7 +8,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, b
-%subplot(3, 3, 2);
+clear all
+s = tf('s');
 num = [1 2 0];
 den = [1 2 1];
 rlocus(num,den)
@@ -15,7 +17,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, c
-%subplot(3, 3, 3);
+clear all
+s = tf('s');
 num = [1 1];
 den = [1 -1 0];
 rlocus(num,den)
@@ -23,7 +26,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, d
-%subplot(3, 3, 4);
+clear all
+s = tf('s');
 num = [1];
 den = [1 2 2 0];
 rlocus(num,den)
@@ -31,7 +35,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, e
-%subplot(3, 3, 5);
+clear all
+s = tf('s');
 num = [1];
 den = [1 3 4 2 0];
 rlocus(num,den)
@@ -39,7 +44,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, f
-%subplot(3, 3, 6);
+clear all
+s = tf('s');
 num = [1 2 2 ];
 den = [1 5 6 0];
 rlocus(num,den)
@@ -47,7 +53,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, g
-%subplot(3, 3, 7);
+clear all
+s = tf('s');
 num = [1 2 0];
 den = [1 2 2];
 rlocus(num,den)
@@ -55,7 +62,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, h
-%subplot(3, 3, 8);
+clear all
+s = tf('s');
 num = [1 0];
 den = [1 -1 0 2];
 rlocus(num,den)
@@ -63,7 +71,8 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 1, i
-%subplot(3, 3, 9);
+clear all
+s = tf('s');
 num = [1 2];
 den = [1 5 8 6 0];
 rlocus(num,den)
@@ -71,8 +80,59 @@ axis equal
 sgrid
 
 %% Homework 7, Problem 4
+s = tf('s');
 num = [20];
-den = [1 5 4 20K 20];
-rlocus(num,den)
-axis equal
-sgrid
+den = [1 5 24];
+rltool(tf(num,den)*(1/s))
+
+%% Midterm Project, Problem 1, a
+clear all
+s = tf('s');
+g = 1/(s^2+s+2);
+h = 1/(s+1);
+gh = minreal(g/(1+g*h));
+[y,t] = step(gh);
+ess=abs(1-y(end)) % error due to step function
+[y,t] = impulse(gh*(1/s^2));
+ess=abs(1-y(end)) % error due to ramp function
+[y,t] = impulse(gh*(1/s^3));
+ess=abs(1-y(end)) % error due to acceleration function
+
+%% Midterm Project, Problem 1, b
+clear all
+s = tf('s');
+g = 1/(s*(s+5));
+h = 5;
+gh = minreal(g/(1+g*h));
+[y,t] = step(gh);
+ess=abs(1-y(end)) % error due to step function
+[y,t] = impulse(gh*(1/s^2));
+ess=abs(1-y(end)) % error due to ramp function
+[y,t] = impulse(gh*(1/s^3));
+ess=abs(1-y(end)) % error due to acceleration function
+
+%% Midterm Project, Problem 1, c
+clear all
+s = tf('s');
+g = 1/(s^2*(s+10));
+h = (s+1)/(s+5);
+gh = minreal(g/(1+g*h));
+[y,t] = step(gh);
+ess=abs(1-y(end)) % error due to step function
+[y,t] = impulse(gh*(1/s^2));
+ess=abs(1-y(end)) % error due to ramp function
+[y,t] = impulse(gh*(1/s^3));
+ess=abs(1-y(end)) % error due to acceleration function
+
+%% Midterm Project, Problem 1, d
+clear all
+s = tf('s');
+g = 1/(s^2*(s+12));
+h = 5*(s+2);
+gh = minreal(g/(1+g*h));
+[y,t] = step(gh);
+ess=abs(1-y(end)) % error due to step function
+[y,t] = impulse(gh*(1/s^2));
+ess=abs(1-y(end)) % error due to ramp function
+[y,t] = impulse(gh*(1/s^3));
+ess=abs(1-y(end)) % error due to acceleration function
