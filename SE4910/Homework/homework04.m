d917cd9235332ccd2e353c30c24358ca4122e178
Steve Mazza <mazzas@cantontechnology.com>
2013-08-04 09:22:58 -0400

    Template work

--- SE4910/Homework/homework04.m
+++ SE4910/Homework/homework04.m
@@ -1,12 +1,12 @@
-%% Homework 7, Problem 1
+%% Homework 7, Problem 1, a
 %subplot(3, 3, 1);
-num = [0];
+num = [1];
 den = [1 0 0 0];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% b
+%% Homework 7, Problem 1, b
 %subplot(3, 3, 2);
 num = [1 2 0];
 den = [1 2 1];
@@ -14,58 +14,65 @@ rlocus(num,den)
 axis equal
 sgrid
 
-%% c
+%% Homework 7, Problem 1, c
 %subplot(3, 3, 3);
-num = [];
-den = [];
+num = [1 1];
+den = [1 -1 0];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% d
+%% Homework 7, Problem 1, d
 %subplot(3, 3, 4);
-num = [];
-den = [];
+num = [1];
+den = [1 2 2 0];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% e
+%% Homework 7, Problem 1, e
 %subplot(3, 3, 5);
-num = [];
-den = [];
+num = [1];
+den = [1 3 4 2 0];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% f
+%% Homework 7, Problem 1, f
 %subplot(3, 3, 6);
-num = [];
-den = [];
+num = [1 2 2 ];
+den = [1 5 6 0];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% g
+%% Homework 7, Problem 1, g
 %subplot(3, 3, 7);
-num = [];
-den = [];
+num = [1 2 0];
+den = [1 2 2];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% h
+%% Homework 7, Problem 1, h
 %subplot(3, 3, 8);
-num = [];
-den = [];
+num = [1 0];
+den = [1 -1 0 2];
 rlocus(num,den)
 axis equal
 sgrid
 
-%% i
+%% Homework 7, Problem 1, i
 %subplot(3, 3, 9);
-num = [];
-den = [];
+num = [1 2];
+den = [1 5 8 6 0];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% Homework 7, Problem 4
+num = [20];
+den = [1 5 4 20K 20];
 rlocus(num,den)
 axis equal
 sgrid
