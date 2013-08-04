6ebe2c2187969d6ea527f2a5470974dc02a44c76
Steve Mazza <mazzas@cantontechnology.com>
2013-08-04 08:46:11 -0400

    Homework 7, Problem 1

---
+++ SE4910/Homework/homework04.m
@@ -0,0 +1,71 @@
+%% Homework 7, Problem 1
+%subplot(3, 3, 1);
+num = [0];
+den = [1 0 0 0];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% b
+%subplot(3, 3, 2);
+num = [1 2 0];
+den = [1 2 1];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% c
+%subplot(3, 3, 3);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% d
+%subplot(3, 3, 4);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% e
+%subplot(3, 3, 5);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% f
+%subplot(3, 3, 6);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% g
+%subplot(3, 3, 7);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% h
+%subplot(3, 3, 8);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
+
+%% i
+%subplot(3, 3, 9);
+num = [];
+den = [];
+rlocus(num,den)
+axis equal
+sgrid
