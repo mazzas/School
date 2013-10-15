0ce7f63c16a04829d05f518d9e26cedceb81b39e
Steve Mazza <mazzas@cantontechnology.com>
2013-10-15 19:05:41 -0400

    Started work on problem 1.
    Stubbed out adj_list, D, and d_bar.

--- SE4960/Homework/HW01/graphSpecs_Mazza.m
+++ SE4960/Homework/HW01/graphSpecs_Mazza.m
@@ -4,8 +4,41 @@ function [ D, L, adj_list, d_bar, diam ] = graphSpecs( A )
 %   adjacency list and Laplacian matrix for A.
 %
 %   INPUTS: A (adjacency matrix)
-%   OUTPUTS: D (vector), L (matrix), adj_list (vector), d_bar (double), 
-%       diam (double)
+%   OUTPUTS: D (vector), L (matrix), adj_list (cell array), 
+%       d_bar (double), diam (double)
+%
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% ADJACENCY LIST
+% We will compute this first since it facilitates some other answers.
+% For each row of the adjacency matrix, build an adjacency list.
+array_size = numel(A(1,:));
+adj_list = cell(array_size);        % dimensoin the array
+for i=1:numel(array_size)
+    adj_list(i,1) = A(i,1);         % store the node value
+    adj_list(i,2) = find(A(i,:));   % store all edges
+end
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% DEGREE
+% For each element in the adjacency list, get the number of elements in the
+%   array.
+D = [];
+for i=1:numel(adj_list)
+    D(end+1) = numel(adj_list{i});
+end
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% D-BAR
+% Compute the average of the degree array.
+d_bar = mean(D);
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% LAPLACIAN MATRIX
+%
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% DIAMETER
 %
 
 
