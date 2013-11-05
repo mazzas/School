e2a720205e837f020f77b0f91c948ac7780b021c
Steve Mazza <mazzas@cantontechnology.com>
2013-11-04 19:51:46 -0500

    Most of 1, 2, & 3.

--- SE3031/Homework/SE3031HW06_Mazza.m
+++ SE3031/Homework/SE3031HW06_Mazza.m
@@ -3,23 +3,23 @@
 close all; clc; clear all;
 
 % a)
-% double precision.
-eps(9460730472580800)
-% single precision.
-eps(single(9460730472580800))
+fprintf('\n\nDouble precision solution is accurate within %d.\n', ...
+    eps(9460730472580800));
+fprintf('Single precision solution is accurate within %d.\n', ...
+    eps(single(9460730472580800)));
 
 % b)
-A = eps(realmax('single'));
-B = eps(realmax('single')/2);
-fprintf('\n\nA: %38.0f\n',A);
-fprintf('B: %38.0f\n',B);
+A = realmax('single');
+B = eps(realmax('single'))/2;
+fprintf('\n\n  A: %38.0f\n',A);
+fprintf('  B: %38.0f\n',B);
 fprintf('A-B: %38.0f\n',A-B);
 
 %% Problem 2
 close all; clc; clear all;
 
 % Estimate 0 with sin().
-sin(4*pi);
+sin(4*pi)
 
 % Estimate number of Taylor series expansions are needed to achieve above.
 X = [ 1, 40 ];
@@ -27,12 +27,65 @@ for i = 1:40
     X(i) = -1^i * (4*pi^(2*i - 1))/factorial(2*i - 1);
 end
 
+figure;
+subplot(2,1,1);
 plot(X);
 hold on;
 line([0,40],[sin(4*pi),sin(4*pi)],'Color','red');
-axis equal;
+axis([0,40,-25,5]);
+subplot(2,1,2);
+plot(X);
+hold on;
+line([0,40],[sin(4*pi),sin(4*pi)],'Color','red');
+axis([3,10,-5,1]);
 %% Problem 3
 close all; clc; clear all;
+
+%a)
+A = [ 6 -3 4; 12 5 -7; -5 2 6 ];
+b = [ 41; -26; 14 ];
+% Direct solution.
+x = A\b
+% Determinant of A.
+s = det(A);
+% Solution using LU decomposition.
+[ R jb ] = rref([ A b ]);
+% Pivot variables.
+p = x(jb);
+% Rank.
+r = length(jb);
+r = rank(A);
+
+%b)
+
+A = [ 0 0 3 5; 3 7 -4 5; 5 -6 7 8; 0 4 0 5 ];
+b = [ 17; 5; 45; 9 ];
+% Direct solution.
+x = A\b
+% Determinant of A.
+s = det(A);
+% Solution using LU decomposition.
+[ R jb ] = rref([ A b ]);
+% Pivot variables.
+p = x(jb);
+% Rank.
+r = length(jb);
+r = rank(A);
+
+%c)
+A = [ 16 2 3 13; 5 11 10 8; 9 7 6 12; 25 27 28 22 ];
+b = [ 41; -26; 14; -64 ];
+% Direct solution (does not work in this case).
+x = A\b
+% Determinant of A.
+s = det(A);
+% Solution using LU decomposition.
+[ R jb ] = rref([ A b ]);
+% Pivot variables.
+p = x(jb);
+% Rank.
+r = length(jb);
+r = rank(A);
 
 %% Problem 4
 close all; clc; clear all;
