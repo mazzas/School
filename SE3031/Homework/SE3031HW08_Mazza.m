6c43d971ce9ae271c63d25fed9817159b0e146f6
Steve Mazza <mazzas@cantontechnology.com>
2013-11-29 14:32:39 -0500

    Completed homework 08.

--- SE3031/Homework/SE3031HW08_Mazza.m
+++ SE3031/Homework/SE3031HW08_Mazza.m
@@ -1,7 +1,7 @@
 %% Homework 08 - Steve Mazza
 %% Problem 1
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
 
 xd = [2 4 6 7];
 yd = [12 15 17 25];
@@ -36,12 +36,127 @@ text(2.25,18,strcat('R^2_3 = ',num2str(Rsq3)));
 
 %% Problem 2
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
+
+xd = [-20 0 20 40 60 80 100 120];
+yd = [4 0.38 0.095 0.032 0.015 0.0078 0.0045 0.0032];
+
+% Plot u=f(T).
+subplot(3,1,1);
+plot(xd,yd,'ro');
+hold on;
+[p,s] = polyfit(xd,yd,1);
+plot(xd,polyval(p,xd));
+title('Linear Fit');
+xlabel('Temperature (Celsius)');
+ylabel('Viscosity');
+
+% Find the quadratic regression.
+subplot(3,1,2);
+plot(xd,yd,'ro');
+hold on;
+[p,s] = polyfit(xd,yd,2);
+plot(xd,polyval(p,xd));
+title('Quadratic Fit');
+xlabel('Temperature (Celsius)');
+ylabel('Viscosity');
+legend('Data points','Regression',0);
+
+% Plot the residuals.
+subplot(3,1,3);
+hold on;
+bar(xd,yd-p(1)*xd-p(2));
+xlim([-20 120]);
+title('Residuals');
+xlabel('Temperature (Celsius)');
+ylabel('ln(y)-ln(y_{est})');
 
 %% Problem 3
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
+
+xd = [0 1 2 10 50 100];
+yd = [100 45 39 22 5 0.5];
+
+% Plot u=f(T).
+subplot(3,1,1);
+plot(xd,yd,'ro');
+hold on;
+title('Linear Interpolation');
+xlabel('Depth');
+ylabel('Light Penetration');
+yy = interp1(xd,yd,30,'linear');
+x = linspace(0,100);
+[p,s] = polyfit(xd,yd,1);
+plot(xd,polyval(p,xd),'-.');
+plot(30,yy,'kd');
+text(32,0,num2str(yy/polyval(p,30)));
+
+subplot(3,1,2);
+plot(xd,yd,'ro');
+hold on;
+title('Spline Interpolation');
+xlabel('Depth');
+ylabel('Light Penetration');
+yy = interp1(xd,yd,30,'spline');
+x = linspace(0,100);
+s = spline(xd,yd,x);
+plot(x,s,'-.');
+plot(30,yy,'kd');
+text(32,10,num2str(yy/s(30)));
+
+subplot(3,1,3);
+plot(xd,yd,'ro');
+hold on;
+title('Cubic Hermite Interpolation');
+xlabel('Depth');
+ylabel('Light Penetration');
+yy = interp1(xd,yd,30,'pchip');
+x = linspace(0,100);
+s = pchip(xd,yd,x);
+plot(x,s,'-.');
+plot(30,yy,'kd');
+text(32,20,num2str(yy/s(30)));
 
 %% Problem 4
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
+
+formatIn = 'dd-mmm-yyyy';
+
+xdate = [datenum('01-May-2013',formatIn) datenum('29-May-2013',formatIn)...
+    datenum('26-Jun-2013',formatIn) datenum('24-Jul-2013',formatIn)];
+ysunrise = [4*60+59 4*60+34 4*60+32 4*60+50];
+ysunset = [18*60+53 19*60+19 19*60+32 18*60+21];
+
+[psunrise, ssunrise] = polyfit(xdate,ysunrise,3);
+[psunset,ssunset] = polyfit(xdate,ysunset,3);
+
+% Calculate sunset on the 4th of July.
+jfour = datenum('04-Jul-2013',formatIn);
+jfoursunsettime = polyval(psunset,jfour);
+jfoursunsethour = floor(jfoursunsettime/60);
+jfoursunsetminute = rem(fix(jfoursunsettime),60);
+fprintf('\n\nSunset on July 4th should be %d hr %d min.\n', ...
+    jfoursunsethour,jfoursunsetminute);
+
+% Calculate date and time of earliest sunrise.
+x = min(xdate):1:max(xdate);
+yallsunrises = polyval(psunrise,x);
+earliestsunrisetime = min(yallsunrises);
+earliestsunrisetimehour = floor(earliestsunrisetime/60);
+earliestsunrisetimeminute = rem(fix(earliestsunrisetime),60);
+earliestsunrisedate = ...
+    datestr(x(find(yallsunrises==earliestsunrisetime)));
+fprintf('The earliest sunrise occurs on %s at %d hr %d min.\n', ...
+    earliestsunrisedate,earliestsunrisetimehour,earliestsunrisetimeminute);
+
+% Calculate date and length of the longest day.
+yallsunsets = polyval(psunset,x);
+alldaylengths = yallsunsets - yallsunrises;
+longestday = max(alldaylengths);
+longestdayhour = floor(longestday/60);
+longestdayminute = rem(fix(longestday),60);
+longestdaydate = datestr(x(find(alldaylengths == longestday)));
+fprintf('The longest day occurs on %s and lasts %d hr %d min.\n', ...
+    longestdaydate,longestdayhour,longestdayminute);
