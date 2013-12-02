76f79ea7b82f25624a4681e92345f1dbe3690bb0
Steve Mazza <mazzas@cantontechnology.com>
2013-12-01 20:44:32 -0500

    Created template.

--- SE3031/Homework/SE3031HW09_Mazza.m
+++ SE3031/Homework/SE3031HW09_Mazza.m
@@ -1,16 +1,16 @@
 %% Homework 09 - Steve Mazza
 %% Problem 1
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
 
 %% Problem 2
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
 
 %% Problem 3
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
 
 %% Problem 4
 %
-clc; clear all; close all; warning off;
+clc; clear all; close all;
