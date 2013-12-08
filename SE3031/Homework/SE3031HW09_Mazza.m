bd8949d0f466beae5ae847ba3b9e4d8e21c9ae06
Steve Mazza <mazzas@cantontechnology.com>
2013-12-08 12:44:08 -0500

    Problems 1-3 completed.

--- SE3031/Homework/SE3031HW09_Mazza.m
+++ SE3031/Homework/SE3031HW09_Mazza.m
@@ -54,6 +54,55 @@ legend('raw data','speed(diff)','speed(grad)','acceleration',0);
 %
 clc; clear all; close all;
 
+% Define data sets.
+x = [ 0 1 2 3 4 5 6 7 8 9 ];                        % days
+y = [ .33 .24 .17 .12 .09 .06 .04 .03 .02 .01 ];    % failure desnity
+
+% Calculate probability of failure.
+F_t = zeros(1,numel(x)-1);
+for i = 2:numel(x)
+    F_t(i-1) = trapz(x(1:i),y(1:i));
+end
+
+% Calculate reliability.
+R_t = 1-F_t;
+
+% Plot some data.
+subplot(3,1,1);
+plot(x,y,'-');
+title('Days vs. Failure Density');
+xlabel('days');
+ylabel('failure density');
+
+subplot(3,1,2);
+plot(x(1:end-1),F_t,'-');
+title('Days vs. Failure Probability');
+xlabel('days');
+ylabel('failure probability');
+
+subplot(3,1,3);
+plot(x(1:end-1),R_t,'-');
+title('Days vs. Reliability');
+xlabel('days');
+ylabel('reliabilty');
+
+% Determine the probability of survival on the interval 2..6 days.
+fprintf(strcat('\n\nThe probability of survival for at least 2 days, ', ...
+    'but not more than 6 days is %1.4f.\n'), trapz(x(3:7),y(3:7)));
+
 %% Problem 4
 %
 clc; clear all; close all;
+syms x y z;
+
+% Define anonymous functions.
+f2 = @(x,y) x^2-3*y^2+x*y^3;
+f3 = @(x,y,z) x^3-2*y*z;
+
+% Calculate double integral.
+q2 = integral2(f2(x,y),0,4,-2,2);
+
+% Calculate triple integral.
+q3 = integral3(f3(x,y,z),-1,3,0,6,-4,4);
+
+% Plot the double integral.
